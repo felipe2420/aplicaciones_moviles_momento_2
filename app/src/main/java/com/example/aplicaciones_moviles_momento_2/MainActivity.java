3dacd1b0a856e076eaad65723d8d4ee83e30f390
Andres Duque <andresduke024@gmail.com>
2023-06-06 22:54:36 -0500

    Initial commit

---
+++ app/src/main/java/com/example/aplicaciones_moviles_momento_2/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.aplicaciones_moviles_momento_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
